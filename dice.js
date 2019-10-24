3c5184768f2495408c912cc8e5f1982acef47b5d
Mizan <mizan_98@hotmail.co.uk>
2019-10-24 15:31:56 +0100

    functions fixed, retry button content added

--- dice.js
+++ dice.js
@@ -1,14 +1,29 @@
-let score = [0]
+let score = 0
 const diceRoll = document.getElementById("roll")
 const diceImg = document.getElementById("dice-img")
 let gameBegin = true
 
 diceRoll.addEventListener("click", ()=>{
-    
-    if(gameBegin){
-        let dice = Math.floor(Math.random() * 6) + 1;
-        diceImg.src = "dice-" + dice + ".png"
+    document.getElementById("playermsg").innerHTML = `Good Luck`
+    let dice = Math.floor(Math.random() * 6) + 1;
+    diceImg.src = "dice-" + dice + ".png"
+    score += dice
+    if (dice > 1){ 
+        document.getElementById("player1score").innerHTML = `score: ${score}`
     }
+    if (score >= 20){
+        let winnerMsg = document.getElementById("playermsg")
+        winnerMsg.textContent = "You WIN!!"
+        score = 0
+        diceRoll.textContent = "retry"
+    }
+
+    else if (dice == 1 && score < 20){
+        let loserMsg = document.getElementById("playermsg")
+        loserMsg.textContent = "you lose"
+        score = 0
+        diceRoll.textContent = "retry"
+    } 
 })
 
 
