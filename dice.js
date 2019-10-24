25a882236d7be944e0f5d701b2569d2b455f8411
Mizan <mizan_98@hotmail.co.uk>
2019-10-24 10:53:03 +0100

    dice roll condition added

--- dice.js
+++ dice.js
@@ -1,9 +1,16 @@
 let score = [0]
 const diceRoll = document.getElementById("roll")
 const diceImg = document.getElementById("dice-img")
+let gameBegin = true
 
 diceRoll.addEventListener("click", ()=>{
-    if()
+    
+    if(gameBegin){
+        let dice = Math.floor(Math.random() * 6) + 1;
+        diceImg.style.display == "none"
+        diceImg.style.display = "block"
+        diceImg.src = "dice-" + dice + ".png"
+    }
 })
 
 
